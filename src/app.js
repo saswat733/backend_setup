3675dbb56bb1fc435f4bf278d91f994cdad64a31
saswat <saswatsingh95747@gmail.com>
2024-01-05 20:21:17 +0530

    complete router and controller setup

--- src/app.js
+++ src/app.js
@@ -9,9 +9,21 @@ app.use(cors({
     credentials:true,
 }))
 
-
+//middlerwares
 app.use(express.json({limit:"10kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))     //public assets for storing files
 app.use(cookieParser())
+
+
+//routes importing
+import userRouter from './routes/user.routes.js'
+
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+//http://localhost:8000/api/v1/users/register
+
 export {app}
+
